d9658fb912aedbd2b53574d609130d3ce057a89e
Arie <arieakouka@hotmail.com>
2020-06-25 19:16:15 +0300

    update v7 ssl certificate

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const fs = require('fs');
 require("dotenv").config();
 
 var options = {
-    key: fs.readFileSync('./key.pem', 'utf8'),
-    cert: fs.readFileSync('./server.crt', 'utf8')
+    key: fs.readFileSync('./certs/server-key.pem', 'utf8'),
+    cert: fs.readFileSync('./certs/server-csr.pem', 'utf8')
 };
 
 const IP_ADDRESS = '3.12.111.177'
