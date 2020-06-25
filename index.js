cafc592acbeccc9467fda7b2418494f16b2efe7d
Arie <arieakouka@hotmail.com>
2020-06-25 17:26:32 +0300

    add ssl certificate

--- index.js
+++ index.js
@@ -8,8 +8,14 @@ var nodemailer = require('nodemailer');
 var jwt = require('jsonwebtoken');
 var ObjectId = require('mongodb').ObjectID;
 var path = require('path');
-
+var https = require('https');
+const fs = require('fs');
 require("dotenv").config();
+
+var options = {
+    key: fs.readFileSync('./certs/server-key.pem'),
+    cert: fs.readFileSync('./certs/server-cert.pem'),
+};
 
 const IP_ADDRESS = '3.12.111.177'
 const EMAIL_SECRET = 'asdf1093KMnzxcvnkljvasdu09123nlasdasdf';
@@ -828,8 +834,13 @@ MongoClient.connect(url, {useNewUrlParser: true}, function (err, client) {
 
 
         //Start Web Server
-        app.listen(3000, () => {
-            console.log('Connected to MongoDB Server, WebService running on port 3000');
-        })
+        // app.listen(3000, () => {
+        //     console.log('Connected to MongoDB Server, WebService running on port 3000');
+        // })
+        const port = 3000;
+        https.createServer(options, app).listen(port, function(){
+            // console.log("Express server listening on port " + port);
+            console.log('Connected to MongoDB Server, WebService running on port ' + port);
+        });
     }
 })
